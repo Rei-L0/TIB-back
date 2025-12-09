4d05ab61d6cf8611d05281256f8a51d18cf57805
Rei-L0 <qkrdusgn00@naver.com>
2025-12-09 09:52:51 +0900

    feat: Initialize new Spring Boot project with Gradle, including essential configuration and wrapper files.

---
+++ src/main/java/com/tib/TibApplication.java
@@ -0,0 +1,13 @@
+package com.tib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tib/TibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
